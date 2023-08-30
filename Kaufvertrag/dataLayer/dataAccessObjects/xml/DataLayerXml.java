15a510b3064c9cca49bb05cc4651ff98c547f151
Justin Rauch <justin1122002@gmail.com>
2023-08-30 10:02:35 +0200

    autogenerated methodes

--- Kaufvertrag/dataLayer/dataAccessObjects/xml/DataLayerXml.java
+++ Kaufvertrag/dataLayer/dataAccessObjects/xml/DataLayerXml.java
@@ -1,5 +1,22 @@
 package Kaufvertrag.dataLayer.dataAccessObjects.xml;
 
+import Kaufvertrag.businessObjects.IVertragspartner;
+import Kaufvertrag.businessObjects.IWare;
+import Kaufvertrag.dataLayer.dataAccessObjects.IDao;
+import Kaufvertrag.dataLayer.dataAccessObjects.IDataLayer;
+
 public class DataLayerXml implements IDataLayer {
+
+    @Override
+    public IDao<IVertragspartner, String> getDaoVertragspartner() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDaoVertragspartner'");
+    }
+
+    @Override
+    public IDao<IWare, Long> getDaoWare() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDaoWare'");
+    }
     
 }
