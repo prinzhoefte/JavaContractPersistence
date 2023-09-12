4c0c1fe744c42d0b3e8e8460540d192964d33a85
langeflx7 <langeflx7@gmail.com>
2023-09-12 23:56:03 +0200

    added documentation comments

--- Kaufvertrag/dataLayer/dataAccessObjects/DataLayerManager.java
+++ Kaufvertrag/dataLayer/dataAccessObjects/DataLayerManager.java
@@ -6,19 +6,33 @@ import Kaufvertrag.dataLayer.dataAccessObjects.sqlite.DataLayerSqlite;
 import Kaufvertrag.dataLayer.dataAccessObjects.xml.DataLayerXml;
 import Kaufvertrag.presentationLayer.exceptions.DaoException;
 
+/**
+ * This class manages the selection and creation of the data access layer based on the chosen persistence type (xml or sqlite).
+ */
 public class DataLayerManager {
     private static final DataLayerManager instance = new DataLayerManager();
     private String persistenceType;
 
     private DataLayerManager() {
-        System.out.println("Datalayermanager created ...");
+        System.out.println("DataLayerManager created...");
         readPersistenceType();
     }
 
-    public static DataLayerManager getInstance(){
+    /**
+     * Get the instance of DataLayerManager (Singleton pattern).
+     *
+     * @return The DataLayerManager instance.
+     */
+    public static DataLayerManager getInstance() {
         return instance;
     }
 
+    /**
+     * Get the appropriate data access layer based on the chosen persistence type.
+     *
+     * @return An instance of IDataLayer for the chosen persistence type.
+     * @throws DaoException If an invalid or unsupported persistence type is selected.
+     */
     public IDataLayer getDataLayer() throws DaoException {
         switch (persistenceType) {
             case "sqlite":
@@ -32,19 +46,22 @@ public class DataLayerManager {
         }
     }
 
+    /**
+     * Read and set the persistence type from user input.
+     *
+     * @return The chosen persistence type.
+     */
     private String readPersistenceType() {
-
         String persistenceType = "";
         Scanner scanner = new Scanner(System.in);
 
         do {
             System.out.println("Choose Persistence Type: XML or SQLite");
             persistenceType = scanner.nextLine();
-        } while(!persistenceType.equalsIgnoreCase("xml") && !persistenceType.equalsIgnoreCase("sqlite"));
+        } while (!persistenceType.equalsIgnoreCase("xml") && !persistenceType.equalsIgnoreCase("sqlite"));
 
         scanner.close();
 
         return this.persistenceType = persistenceType;
     }
-
 }
--- Kaufvertrag/dataLayer/dataAccessObjects/sqlite/ConnectionManager.java
+++ Kaufvertrag/dataLayer/dataAccessObjects/sqlite/ConnectionManager.java
@@ -6,23 +6,33 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * This class manages database connections for SQLite.
+ */
 public class ConnectionManager {
 
     private static final String CLASSNAME = "org.sqlite.JDBC";
-    private static final String CONNECTIONSTRING = "jdbc:sqlite:%FILE%"; // URL*
+    private static final String CONNECTIONSTRING = "jdbc:sqlite:%FILE%"; // includes file path
 
     private Connection existingConnection;
     private boolean classLoaded = false;
 
+    /**
+     * Get a new database connection.
+     *
+     * @return A new database connection.
+     */
     public Connection getNewConnection() {
 
         if(!classLoaded) {
 
             try {
+                // Load the SQLite JDBC driver class.
                 Class.forName(CLASSNAME);
                 classLoaded = true;
 
                 try {
+                    // Establish a new database connection.
                     return existingConnection = DriverManager.getConnection(CONNECTIONSTRING);
                 } catch (SQLException e) {
                     e.printStackTrace();
@@ -32,6 +42,7 @@ public class ConnectionManager {
             }
         } else {
             try {
+                // Close the existing connection and return a new one.
                 existingConnection.close();
                 return existingConnection = DriverManager.getConnection(CONNECTIONSTRING);
             } catch (SQLException e) {
@@ -41,24 +52,39 @@ public class ConnectionManager {
         return null;
     }
 
+    /**
+     * Get the existing database connection.
+     *
+     * @return The existing database connection.
+     */
     public Connection getExistingConnection() {
         return existingConnection;
     }
 
-    // catch on call with dao exception
+    /**
+     * Close the result set, statement, and connection.
+     *
+     * @param resultSet  The result set to close.
+     * @param statement  The statement to close.
+     * @param connection The connection to close.
+     * @throws SQLException If an SQL error occurs during the close operation.
+     */
     public void close(ResultSet resultSet, Statement statement, Connection connection) throws SQLException {
 
         try {
+            // Close the statement.
             statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             try {
+                // Close the result set.
                 resultSet.close();
             } catch(SQLException e) {
                 e.printStackTrace();
             } finally {
                 try {
+                    // Close the connection.
                     connection.close();
                 } catch(SQLException e) {
                     e.printStackTrace();
@@ -66,5 +92,4 @@ public class ConnectionManager {
             }
         }
     }
-    
 }
