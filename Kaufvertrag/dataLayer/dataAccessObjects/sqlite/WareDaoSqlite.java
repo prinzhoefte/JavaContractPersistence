ccc8f645fae7b6a278a1eb43c6a20e992f816403
langeflx7 <langeflx7@gmail.com>
2023-09-13 11:18:47 +0200

    updated waredaosqlite

--- Kaufvertrag/dataLayer/dataAccessObjects/sqlite/WareDaoSqlite.java
+++ Kaufvertrag/dataLayer/dataAccessObjects/sqlite/WareDaoSqlite.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Kaufvertrag.businessObjects.IWare;
+import Kaufvertrag.dataLayer.Formatter;
 import Kaufvertrag.dataLayer.businessObjects.Ware;
 import Kaufvertrag.dataLayer.dataAccessObjects.IDao;
 import Kaufvertrag.presentationLayer.exceptions.DaoException;
@@ -25,10 +26,14 @@ public class WareDaoSqlite implements IDao<IWare, Long> {
             Connection connection = connectionManager.getNewConnection();
 
             // Define the SQL query to insert a new 'ware' record.
-            String sql = "INSERT INTO ware (bezeichnung, preis) VALUES (?, ?)";
+            String sql = "INSERT INTO ware (bezeichnung, beschreibung, preis, besonderheiten, maengel) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement statement = connection.prepareStatement(sql);
+
             statement.setString(1, objectToInsert.getBezeichnung());
-            statement.setString(2, objectToInsert.getPreis());
+            statement.setString(2, objectToInsert.getBeschreibung());
+            statement.setString(3, objectToInsert.getPreis());
+            statement.setString(4, Formatter.ListToString(objectToInsert.getBesonderheiten()));
+            statement.setString(4, Formatter.ListToString(objectToInsert.getMaengel()));
 
             // Execute the insert query.
             statement.executeUpdate();
@@ -54,20 +59,26 @@ public class WareDaoSqlite implements IDao<IWare, Long> {
             // Define the SQL query to select a 'ware' record by its ID.
             String sql = "SELECT * FROM ware WHERE id = ?";
             PreparedStatement statement = connection.prepareStatement(sql);
+
             statement.setLong(1, id);
 
             // Execute the select query.
             ResultSet resultSet = statement.executeQuery();
 
             if (resultSet.next()) {
+
                 // Retrieve data from the result set.
                 String bezeichnung = resultSet.getString("bezeichnung");
+                String beschreibung = resultSet.getString("beschreibung");
                 String preis = resultSet.getString("preis");
+                String besonderheiten = resultSet.getString("besonderheiten");
+                String maengel = resultSet.getString("maengel");
 
-                // Close the connection.
-                connectionManager.close(resultSet, statement, connection);
+                ware = new Ware(bezeichnung, preis);
 
-                return new Ware(bezeichnung, preis);
+                ware.setBeschreibung(beschreibung);
+                ware.setBesonderheiten(besonderheiten);
+                ware.setMaengel(maengel);
             }
 
             // Close the connection.
@@ -98,12 +109,21 @@ public class WareDaoSqlite implements IDao<IWare, Long> {
             ResultSet resultSet = statement.executeQuery();
 
             while (resultSet.next()) {
+
                 // Retrieve data from the result set.
                 String bezeichnung = resultSet.getString("bezeichnung");
+                String beschreibung = resultSet.getString("beschreibung");
                 String preis = resultSet.getString("preis");
+                String besonderheiten = resultSet.getString("besonderheiten");
+                String maengel = resultSet.getString("maengel");
 
                 // Create a 'Ware' object and add it to the list.
                 Ware ware = new Ware(bezeichnung, preis);
+
+                ware.setBeschreibung(beschreibung);
+                ware.setBesonderheiten(besonderheiten);
+                ware.setMaengel(maengel);
+
                 wareList.add(ware);
             }
 
@@ -125,11 +145,14 @@ public class WareDaoSqlite implements IDao<IWare, Long> {
             Connection connection = connectionManager.getNewConnection();
 
             // Define the SQL query to update a 'ware' record by its ID.
-            String sql = "UPDATE ware SET bezeichnung = ?, preis = ? WHERE id = ?";
+            String sql = "UPDATE ware SET bezeichnung = ?, beschreibung = ?, preis = ?, besonderheiten = ?, maengel = ? WHERE id = ?";
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setString(1, objectToUpdate.getBezeichnung());
-            statement.setString(2, objectToUpdate.getPreis());
-            statement.setLong(3, objectToUpdate.getId());
+            statement.setString(2, objectToUpdate.getBeschreibung());
+            statement.setString(3, objectToUpdate.getPreis());
+            statement.setString(4, Formatter.ListToString(objectToUpdate.getBesonderheiten()));
+            statement.setString(5, Formatter.ListToString(objectToUpdate.getMaengel()));
+            statement.setLong(6, objectToUpdate.getId());
 
             // Execute the update query.
             statement.executeUpdate();
